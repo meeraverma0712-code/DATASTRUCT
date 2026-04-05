749731d349b5c3ae5c797b98048df1058081f119
Meera Verma <meeraverma0712@gmail.com>
2026-04-05 21:43:56 +0530

    spiral matrix

--- 2D Arrays/spiralmatrix.cpp
+++ 2D Arrays/spiralmatrix.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 
    void spiralMatrix(int mat[][4], int n , int m){
-    int n =3 , m = 4;
     int srow=0 ,scol=0;
     int erow=n-1 ,ecol = m-1;
 
-    while(   ){
-
-    
-    for(int j=scol;j<=ecol;j++){
-       cout<<mat [srow][j]<< " ";
+    while( srow<=erow && scol<=ecol ){
+     
+      for(int j=scol;j<=ecol;j++){
+         cout<<mat[srow][j]<< " ";
     }
 
     for (int i=srow+1;i<=erow ;i++){
+       if(srow==erow){
+            break;
+         }
        cout <<mat[i] [ecol] << " "  ;
     }
        for (int j=ecol-1;j<=srow+1 ;j++){
+          if(scol==ecol){
+            break;
+         }
             cout <<mat[erow] [j] << " "  ;
     }
        for (int i=erow-1;i<=srow+1;i++){
@@ -28,12 +32,14 @@ using namespace std;
     erow--;
     ecol--;
 }
+cout << endl;
     }
 
     int main(){
         int matrix[][4] = {{1,2,3,4},
                             {5,6,7,8},
                             {9,10,11,12},
-                            {13,14,15,16}}
+                            {13,14,15,16}};
+      spiralMatrix(matrix , 4, 4);
     return 0;
 }
