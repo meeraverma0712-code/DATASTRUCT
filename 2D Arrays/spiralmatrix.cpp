d8bfc9f4ab2b12353f4a0a8ed3e17a31247b1ee9
Meera Verma <meeraverma0712@gmail.com>
2026-04-06 11:28:37 +0530

    corrected the spiral matrix code

--- 2D Arrays/spiralmatrix.cpp
+++ 2D Arrays/spiralmatrix.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
-
-
-   void spiralMatrix(int mat[][4], int n , int m){
-    int srow=0 ,scol=0;
+    void spiralMatrix(int mat[][4], int n , int m){
+    int srow=0 , scol=0;
     int erow=n-1 ,ecol = m-1;
 
     while( srow<=erow && scol<=ecol ){
@@ -15,17 +13,18 @@ using namespace std;
     for (int i=srow+1;i<=erow ;i++){
        if(srow==erow){
             break;
-         }
-       cout <<mat[i] [ecol] << " "  ;
+
+         } 
+       cout <<mat[i][ecol] << " "  ;
     }
-       for (int j=ecol-1;j<=srow+1 ;j++){
+       for (int j=ecol-1;j>=scol ;j--){
           if(scol==ecol){
             break;
          }
-            cout <<mat[erow] [j] << " "  ;
+            cout <<mat[erow][j] << " "  ;
     }
-       for (int i=erow-1;i<=srow+1;i++){
-            cout <<mat[i] [scol] << " "  ;
+       for (int i=erow-1;i>=srow+1;i--){
+            cout <<mat[i][scol] << " "  ;
     }
     srow++;
     scol++;
@@ -37,9 +36,9 @@ cout << endl;
 
     int main(){
         int matrix[][4] = {{1,2,3,4},
-                            {5,6,7,8},
-                            {9,10,11,12},
-                            {13,14,15,16}};
-      spiralMatrix(matrix , 4, 4);
+                           {5,6,7,8},
+                           {9,10,11,12},
+                           {13,14,15,16}};
+      spiralMatrix(matrix,4,4);
     return 0;
 }
