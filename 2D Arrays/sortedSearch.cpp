723d3ea505c062827d81d09e3273860adbfe6c45
Meera Verma <meeraverma0712@gmail.com>
2026-04-08 08:13:28 +0530

    2d arrays finished

---
+++ 2D Arrays/sortedSearch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int search(int mat[][4], int n,int m ,int key){
+    
+    int sum = 0;
+
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++)
+
+          if(mat[i][j] == key){
+            cout<< "key found"<< endl ;
+          }
+          else 
+           return -1;
+        }
+      }
+int main(){
+     int matrix[4][4]={{10,20,30,40},
+                    {15,25,35,45},
+                    {27,29,37,48},
+                    {32,33,39,50}};
+    search( matrix , 4,4 , 100);
+   return 0;
+}
