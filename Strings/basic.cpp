c66f69fe7ac397150fb15c323f6ea60eef91fac8
Meera Verma <meeraverma0712@gmail.com>
2026-04-08 17:24:28 +0530

    strings

---
+++ Strings/basic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+    // char sentence[30];
+    // cin.getline(sentence , 30);
+
+    // cout<<" this is your sentence : "<< sentence<<  endl;
+
+
+    // upper case
+
+    void ToUpper( char word[] , int n ){
+        for (int i =0 ; i< n ; i++){
+            char ch = word[i];
+            if(ch>= 'A'&& ch<= 'Z'){
+
+                continue;
+            }
+            else{
+                word[i]=ch - 'a'+'A';
+            }
+        }
+
+    }
+int main(){
+      char word[]= "sanbdjhedbsd";
+      ToUpper(word, 12);
+      cout<< word<< endl;
+     
+
+    
+    return 0;
+}
---
+++ Strings/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool isPalindrome(char word[], int n){
+    int st = 0 ; int end = n-1;
+    while(st<end){
+        if(word[st++] != word[end--]){
+         cout << "is not palindrome ";
+        return false;
+        }
+       
+    }
+     cout << "is palindrome ";
+    return true;
+  }
+   
+int main(){
+
+    char word[]= "racecar" ;
+    isPalindrome(word, strlen(word));
+   
+    return 0;
+}
