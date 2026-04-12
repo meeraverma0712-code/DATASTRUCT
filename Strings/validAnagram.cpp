cefa2f898d0bce29296cc35c39793e12a4f46d85
Meera Verma <meeraverma0712@gmail.com>
2026-04-12 18:28:24 +0530

    strings lecture qs over

---
+++ Strings/validAnagram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool isAnagram(string str1 , string str2){
+    if (str1.length() != str2.length()){
+        cout<< "invalid anagram" << endl;
+        return false;
+    }
+      int count[26]= {0};
+    for (int i = 0; i<str1.length();i++){
+        count[str1[i]-'a']++;
+    }
+    for (int i = 0; i<str2.length();i++){
+         if( count[str2[i]-'a']==0){
+          cout<< "invalid anagram" << endl;
+          return false;
+       }
+        count[str2[i]-'a']--;
+    }
+    cout<<"valid anagram"<< endl;
+     return true;
+}
+int main(){
+  isAnagram( "god" , "dog" );
+  return 0;
+}
