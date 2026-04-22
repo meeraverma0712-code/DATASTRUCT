25fa0ddff6d482ff176f2e5aab629f1c116f0d52
Meera Verma <meeraverma0712@gmail.com>
2026-04-22 10:47:47 +0530

    practice qs

--- OOPs/constructor.cpp
+++ OOPs/constructor.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 using namespace std;
-
-    
-    class Car {
+   class Car {
     string name ;
     string colour;
 
     public:
       Car(){
-        cout << "constructor has been called.object is being created...\n";
+        cout << "constructor without parameters...\n";
       }
-     void start(){
-      cout << "car has started...\n";
-     }
+      Car(string name , string colour){
+        cout << "constructor with parameters.\n";
+        this->name = name;
+        this->colour = colour;
+      }
+      void start(){
+        cout << "car is starting...\n";
+      }
+      void stop(){
+        cout << "car is stopped\n";
+      }
 
-     void stop(){
-      cout << "car has stopped\n";
-     }
-    };
+      string getname(){
+        return name;
+      }
+  };
    
     int main(){
-    Car c1;
+    Car c0;
+    Car c1("maruti" , "black");
+    Car C2("suv 700" , "black");
+
     
      return 0;
 }
---
+++ OOPs/practiceQ.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+   class User {
+private:
+    int id;
+    string password;
+
+public:
+    string username;
+
+    User(int id) {
+        this-> id = id; 
+        
+    }
+    string getPassword(){
+     return password;
+   }
+
+  void setPassword(string password){
+    this-> password = password;
+
+    }
+  };
+   
+
+    int main(){
+    User user1(101);
+    user1.username = "MeeH";
+    user1.setPassword("abdc");
+
+    cout<< "username : "<< user1.username << endl;
+    cout << "password = " << user1.getPassword() << endl;
+
+     
+     return 0;
+}
