f5459af6216fccdeb9939cc1fcf04b25fdbc1bf2
Meera Verma <meeraverma0712@gmail.com>
2026-04-24 17:49:57 +0530

    runtime poly

--- OOPs/polymorphism.cpp
+++ OOPs/polymorphism.cpp
@@ -24,16 +24,28 @@ using namespace std;
         real = r;
         img = i ;
 }
-
-    void showNum( ){
+     void showNum( ){
         cout << real << "+" <<img <<"i"<<endl;    }
-  };
+  
+
+  void operator + (complex &obj){
+    int resReal=this->real + obj.real ;
+    int resImg =this->img + obj.img ;
+    complex c3(resReal , resImg);
+    cout << "res :" ;
+     c3.showNum();
+  }
+};
+
+
 
    int main(){
    complex  c1(2,3);
    complex c2 (1,2);
    c1.showNum();
    c2.showNum();
+
+   c1 + c2;
     return 0;
 }
 
