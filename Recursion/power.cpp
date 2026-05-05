9140b16c029e4077a64fc82d4e4653373812bf84
Meera Verma <meeraverma0712@gmail.com>
2026-05-05 10:57:44 +0530

    recursive x power n

---
+++ Recursion/power.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// without recursion
+  int power(int x , int n){
+   int pow = 1;
+   for(int i=0; i<n ; i++){
+   pow = pow * x;
+   }
+   return pow;
+   }
+
+ // with recursion 
+  int recurPower(int x , int n){
+    if(n==0){
+      return 1;
+    }
+  int halfPow = power(x , n/2);
+  int halfPowSq = halfPow* halfPow;
+
+  if(n%2 != 0){
+    // odd
+    return x * halfPowSq;
+}
+  return halfPowSq;
+}
+int main(){
+cout << recurPower(2,5);
+  return 0;
+}
