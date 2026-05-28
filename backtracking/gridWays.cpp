7d62331dacd85f5285b72b3d6bc474067715e884
Meera Verma <meeraverma0712@gmail.com>
2026-05-28 13:07:25 +0530

    gridWays

---
+++ backtracking/gridWays.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int gridWays(int r , int c , int n , int m){
+    if (r == n-1 && c == m-1){
+        return 1;
+    }
+    if (r>=n || c>=m)
+    return 0;
+
+    int val1 = gridWays(r, c+1 , n , m);
+     int val2 = gridWays(r+1, c , n , m);
+     return val1 + val2;
+}
+
+
+int main(){
+  int n = 3;
+  int m =3;
+  cout << gridWays(0,0,n,m);
+  return 0;
+}
