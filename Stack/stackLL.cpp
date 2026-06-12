82e22762955597e37fa26b790f72d73abf718d24
Meera Verma <meeraverma0712@gmail.com>
2026-06-12 22:28:33 +0530

    ..

---
+++ Stack/stackLL.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+   
+
+
+int main(){
+   
+    while(!s.isEmpty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    return 0;
+
+}
