670a6b40f0cfbad379b7157c49eddb227c9e978a
Meera Verma <meeraverma0712@gmail.com>
2026-06-16 10:46:33 +0530

    ..

--- linkedList/printll.cpp
+++ linkedList/printll.cpp
@@ -1,71 +1,84 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-   
-class Node {
-    public:
+
+class Node
+{
+public:
     int data;
-    Node* next;     
+    Node *next;
 
 public:
-    Node(int val){
+    Node(int val)
+    {
         data = val;
         next = NULL;
     }
 };
 
-class List{
-    Node* head;
-    Node* tail;
+class List
+{
+    Node *head;
+    Node *tail;
 
 public:
-    List(){
+    List()
+    {
         head = NULL;
         tail = NULL;
     }
 
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
 
-void push_front(int val){
-    Node* newNode = new Node(val);
-
-    if(head == NULL){
-        head = tail = newNode;
-    } else{
-        newNode->next = head;
-        head = newNode;
-     }
-}
-
-
-void push_back(int val){
-    Node* newNode = new Node(val);
-
-    if(head == NULL){
-        head = tail = newNode;
-    } else{
-        tail->next = newNode;
-        tail = newNode;
-     }
-}
-
-void printList(){
-    Node* temp = head;
-    while(temp!= NULL){
-        cout << temp->data << " -> ";
-        temp = temp-> next;
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
     }
-    cout << "NULL\n" ;
-   }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
 };
 
-int main(){
-   List ll;
-   ll.push_front(3);
-   ll.push_front(2);
-   ll.push_front(1);
-   ll.printList();
-   ll.push_back(4);
-   ll.push_back(5);
-   ll.printList();
-   return 0;
+int main()
+{
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
+    ll.printList();
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.printList();
+    return 0;
 }
